c1fca35ce2746225274fd10a0220ca914fbe7596
Marcus Lorenzana <marcus.lorenzana@terazo.com>
2021-08-31 09:25:00 -0500

    laying out app

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, SafeAreaView, StyleSheet, TextInput, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView>
+            <View style={{padding: 30}}>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                    <TextInput placeholder="Course Goal"
+                               style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}/>
+                    <Button title="ADD"/>
+                </View>
+                <View>
+
+                </View>
+            </View>
+
+            <StatusBar style="auto"/>
+        </SafeAreaView>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
