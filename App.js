b531a5afc7de3c4c36a5a31e35ea594be2415546
Marcus Lorenzana <marcus.lorenzana@terazo.com>
2021-09-01 09:11:49 -0500

    deleting, uuidv4

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
 import React, {useState} from 'react';
-import {Button, FlatList, SafeAreaView, StyleSheet, TextInput, View} from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet, View} from 'react-native';
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
     const [courseGoals, setCourseGoals] = useState([]);
 
     const addGoalHandler = (goalTitle) => {
-        setCourseGoals(currentGoals => [...currentGoals, {key: Math.random().toString(), value: goalTitle}]);
+        setCourseGoals(currentGoals => [...currentGoals, {id: uuidv4(), value: goalTitle}]);
+    };
+
+    const removeGoalHandler = (goalId) => {
+        setCourseGoals(currentGoals => {
+            console.log('goalId: ', goalId);
+            return currentGoals.filter(goal => goal.id !== goalId);
+        });
     };
 
     return (
         <SafeAreaView>
             <View style={styles.screen}>
-                <GoalInput submit={addGoalHandler} />
-                <FlatList data={courseGoals} renderItem={itemData => (
-                    <GoalItem title={itemData.item.value}/>
-                )}>
+                <GoalInput submit={addGoalHandler}/>
+                <FlatList
+                    keyExtractor={(item, index) => item.id}
+                    data={courseGoals}
+                    renderItem={itemData => (
+                        <GoalItem
+                            id={itemData.item.id}
+                            onDelete={removeGoalHandler}
+                            title={itemData.item.value}
+                        />
+                    )}>
                 </FlatList>
             </View>
         </SafeAreaView>
